3ca8028dd7d8791f3f68b2d73dcf1e37cb8fad10
the3luckyGamer <77641749+the3luckyGamer@users.noreply.github.com>
2024-09-28 12:05:15 +0100

    Create noblox.ts

---
+++ utils/noblox.ts
@@ -0,0 +1,32 @@
+export function getLogo(group) {
+  return "https://serpent.sf-tech.xyz/smartaccess1.png"
+}
+
+export function getGroup(groupId) {
+  return {
+  "id": 3140707,
+  "name": "Springfield High School",
+  "description": "Welcome to Springfield High School! \n\nOur mission is to create a unique, enjoyable experience, somewhat reflecting school within the UK as best we can. We achieve this by offering top-quality lesson content, implementing new features and ideas daily and making sure all staff follow curriculums set out by SLT. \n\nSHS is an inclusive community, where we welcome everyone and encourage our students to partake in as much activities as possible, as well as engage in their lessons. \n\nWe specialise in Science, which means we have a strong emphasis on STEM related content throughout our year groups. Each of our departments are well managed and our dedicated year team help to ensure student wellbeing is maintained at all times. \n\nIf you have any questions/concerns regarding our school, please contact the Headteacher, Mr Mordaunt",
+  "owner": {
+    "hasVerifiedBadge": false,
+    "userId": 155212891,
+    "username": "AMirkwoodElf",
+    "displayName": "Jake_Mordaunt"
+  },
+  "shout": {
+    "body": "",
+    "poster": {
+      "hasVerifiedBadge": false,
+      "userId": 155212891,
+      "username": "AMirkwoodElf",
+      "displayName": "Jake_Mordaunt"
+    },
+    "created": new Date("2017-03-09T22:35:33.78Z"),
+    "updated": new Date("2024-08-16T17:37:12.297Z")
+  },
+  "memberCount": 1416,
+  "isBuildersClubOnly": false,
+  "publicEntryAllowed": false,
+  "hasVerifiedBadge": false
+}
+}
