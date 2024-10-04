cde92dc7d634a4a38a460bd84afe613ccb6e02a0
the3luckyGamer <77641749+the3luckyGamer@users.noreply.github.com>
2024-10-04 14:01:03 +0100

    Update noblox.js

--- utils/noblox.js
+++ utils/noblox.js
@@ -1,34 +1,26 @@
-export function getLogo(group) {
-  return "https://serpent.sf-tech.xyz/smartaccess1.png"
+import axios from "axios";
+
+export async function getLogo(group) {
+  const { data } = await axios.get(
+    `https://thumbnails.roblox.com/v1/groups/icons?groupIds=${group}&size=150x150&format=Png`
+  );
+
+  return data.data[0].imageUrl;
 }
 
 export function getGroup(groupId) {
   return new Promise((resolve, reject) => {
-    resolve({
-  "id": 3140707,
-  "name": "Springfield High School",
-  "description": "Welcome to Springfield High School! \n\nOur mission is to create a unique, enjoyable experience, somewhat reflecting school within the UK as best we can. We achieve this by offering top-quality lesson content, implementing new features and ideas daily and making sure all staff follow curriculums set out by SLT. \n\nSHS is an inclusive community, where we welcome everyone and encourage our students to partake in as much activities as possible, as well as engage in their lessons. \n\nWe specialise in Science, which means we have a strong emphasis on STEM related content throughout our year groups. Each of our departments are well managed and our dedicated year team help to ensure student wellbeing is maintained at all times. \n\nIf you have any questions/concerns regarding our school, please contact the Headteacher, Mr Mordaunt",
-  "owner": {
-    "hasVerifiedBadge": false,
-    "userId": 155212891,
-    "username": "AMirkwoodElf",
-    "displayName": "Jake_Mordaunt"
-  },
-  "shout": {
-    "body": "",
-    "poster": {
-      "hasVerifiedBadge": false,
-      "userId": 155212891,
-      "username": "AMirkwoodElf",
-      "displayName": "Jake_Mordaunt"
-    },
-    "created": new Date("2017-03-09T22:35:33.78Z"),
-    "updated": new Date("2024-08-16T17:37:12.297Z")
-  },
-  "memberCount": 1416,
-  "isBuildersClubOnly": false,
-  "publicEntryAllowed": false,
-  "hasVerifiedBadge": false
-})
+    axios.get(`https://groups.roblox.com/v1/groups/${groupId}`).then(data => {
+      let body = data.data
+
+      if (body.shout) {
+          body.shout.created = new Date(body.shout.created)
+          body.shout.updated = new Date(body.shout.updated)
+      }
+
+      resolve(body)
+    }).catch(err => {
+      reject(err)
+    })
   })
 }
