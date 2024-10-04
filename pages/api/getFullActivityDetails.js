f6ca0536c6938431162d81c9d2745698db9d3cc2
the3luckyGamer <77641749+the3luckyGamer@users.noreply.github.com>
2024-10-04 21:34:55 +0100

    Update and rename getFullActivityDetails.ts to getFullActivityDetails.js

--- pages/api/getFullActivityDetails.js
+++ pages/api/getFullActivityDetails.js
@@ -1,33 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
 import { getConfig, setConfig } from '@/utils/configEngine'
 import prisma from '@/utils/database';
 import { withPermissionCheck } from '@/utils/permissionsManager'
 import * as noblox from 'noblox.js'
-type Data = {
-	success: boolean
-	data: any
-}
 
 export default withPermissionCheck(handler, 'admin');
 
-export async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
-) {
+export async function handler(req,res) {
 
 	const workspace = await prisma.workspace.findFirst({
 		where: {
 			groupId: 10503931,
 		}
 	});
-	if (!workspace) return res.status(404).json({ success: false, error: 'Workspace not found' });
+	//if (!workspace) return res.status(404).json({ success: false, error: 'Workspace not found' });
 
 	//const roles = await noblox.getRoles(workspace.groupId);
-	const activityconfig = await getConfig('activity', parseInt(req.query.id as string));
+	const activityconfig = await getConfig('activity', 10503931);
 
 	res.status(200).send({
-		data: activityconfig?,
+		data: activityconfig,
 		success: true,
 	});
 }
